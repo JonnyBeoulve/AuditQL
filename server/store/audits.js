9dcce5101710965d40238a29e2aedfde491ad939
JonnyBeoulve <jleack@gmail.com>
2019-02-23 08:46:01 -0800

    Created dummy data for audits in store.

---
+++ server/store/audits.js
@@ -0,0 +1,7 @@
+let audits = [
+    { id: '1', genre: 'Compliance' },
+    { id: '2', genre: 'Financial' },
+    { id: '3', genre: 'Operational' },
+    { id: '4', genre: 'Compliance' },
+    { id: '5', genre: 'Forensic' },
+];
