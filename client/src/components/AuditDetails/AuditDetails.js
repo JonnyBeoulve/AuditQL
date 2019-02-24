7c6711fac2208ca9249535ad5d6fef2bd54c20eb
JonnyBeoulve <jleack@gmail.com>
2019-02-24 15:56:37 -0800

    Added button for returning to All Audits. Improved details styling.

--- client/src/components/AuditDetails/AuditDetails.js
+++ client/src/components/AuditDetails/AuditDetails.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Query } from 'react-apollo';
-import { Card, Heading, Text } from 'rebass';
+import { Box, Button, Card, Flex, Heading, Text } from 'rebass';
+import { LeftArrowCircle } from 'styled-icons/boxicons-regular/LeftArrowCircle';
 
 import { getAuditQuery } from '../../api/queries';
 
 /* This component will use an unordered list to display a list
 of audits. */
-const AuditDetails = ({ id }) => (
+const AuditDetails = ({ id, selectAudit }) => (
     <Query query={getAuditQuery} variables={{ id }}>
         {({ loading, error, data }) => {
             if (loading) return <Card mt={[30, 50]}><Text>Loading...</Text></Card>;
@@ -15,11 +16,18 @@ const AuditDetails = ({ id }) => (
             return (
                 <main>
                     <Card mt={[30, 50]}>
-                        <Heading mb={3}>Audit Details</Heading>
-                        <Text>Title: {data.audit.title}</Text>
-                        <Text>Genre: {data.audit.genre}</Text>
-                        <Text>Assigned Auditor: {data.audit.auditor.name}</Text>
-                        <Text>Assigned Auditor's Specialization: {data.audit.auditor.specialization}</Text>
+                        <Flex width={1} justifyContent={'space-between'}>
+                                <Heading mb={3}>Audit Details</Heading>
+                                <Button bg={'bgPrimary'} fontSize={'1'} onClick={(id) => selectAudit(null)}><LeftArrowCircle size={16} /> Return</Button>
+                        </Flex>
+                        <Text fontSize={'1'} color={'darkgrey'}>Title</Text>
+                        <Text mb={2}>{data.audit.title}</Text>
+                        <Text fontSize={'1'} color={'darkgrey'}>Genre</Text>
+                        <Text mb={2}>{data.audit.genre}</Text>
+                        <Text fontSize={'1'} color={'darkgrey'}>Assigned Auditor</Text>
+                        <Text mb={2}>{data.audit.auditor.name}</Text>
+                        <Text fontSize={'1'} color={'darkgrey'}>Assigned Auditor's Specialization</Text>
+                        <Text mb={2}>{data.audit.auditor.specialization}</Text>
                     </Card>
                 </main>
             );
