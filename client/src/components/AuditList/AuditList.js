a485e174a68e70676344df5b1c78fd1eeae40272
JonnyBeoulve <jleack@gmail.com>
2019-02-24 09:59:51 -0800

    Built AuditList component and added GraphQL related dependencies.

---
+++ client/src/components/AuditList/AuditList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/* This component will use an unordered list to display a list
+of audits. */
+const AuditList = props => {
+    return (
+        <section id="audit-list">
+            <ul id="audit-list-items">
+                <li>Audit Name</li>
+            </ul>
+        </section>
+    )
+}
+
+export default AuditList;
--- client/src/containers/Audits/Audits.js
+++ client/src/containers/Audits/Audits.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { GlobalStyle } from '../../theme';
 
+import AuditList from '../../components/AuditList/AuditList';
+
 /* This container will list all audits. This will temporarily serve as the main container of
 the app until the project is expanded in the future. */
 class Audits extends Component {
@@ -9,6 +11,7 @@ class Audits extends Component {
         <div className="main">
             <GlobalStyle />
             <h1>Audits</h1>
+            <AuditList />
         </div>
     );
   }
