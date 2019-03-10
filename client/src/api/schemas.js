3b04a04d95ad629baa18d84696e7ec6df31f1bc9
JonnyBeoulve <jleack@gmail.com>
2019-03-10 12:12:09 -0700

    Added Yup to project. Built Yup schema for add audit.

---
+++ client/src/api/schemas.js
@@ -0,0 +1,14 @@
+import * as Yup from 'yup';
+
+export const AddAuditSchema = Yup.object().shape({
+  title: Yup.string()
+    .min(5, 'Too short. There is a 5 character minimum.')
+    .max(40, 'Too long. There is a 40 character limit.')
+    .required('Required'),
+  status: Yup.string()
+    .required('Required'),
+  genre: Yup.string()
+    .required('Required'),
+  auditor: Yup.string()
+    .required('Required'),
+});
