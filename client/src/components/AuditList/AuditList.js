b2c19a575b6e2e353ed8d10c42856e6807deb968
JonnyBeoulve <jleack@gmail.com>
2019-03-10 10:26:14 -0700

    Refactored radial chart into its own component.

--- client/src/components/AuditList/AuditList.js
+++ client/src/components/AuditList/AuditList.js
@@ -2,11 +2,10 @@ import React from 'react';
 import { Query } from 'react-apollo';
 import { Box, Card, Flex, Heading, Text } from 'rebass';
 import { RightArrowCircle } from 'styled-icons/boxicons-regular/RightArrowCircle';
-import { RadialChart } from 'react-vis';
 import { DocumentText } from 'styled-icons/typicons/DocumentText';
 import { Table } from 'styled-icons/boxicons-regular/Table';
-import '../../../node_modules/react-vis/dist/style.css';
 
+import GenreRadialChart from '../Charts/GenreRadialChart/GenreRadialChart';
 import { getAuditsQuery } from '../../api/queries';
 import { AuditListText } from './auditListStyling';
 import { DefaultButton } from '../../theme/base';
@@ -19,28 +18,6 @@ const AuditList = props => (
         {({ loading, error, data }) => {
             if (loading) return <Card mt={[30, 50]}><Text>Loading...</Text></Card>;
             if (error) return <Card mt={[30, 50]}><Text>Error! {error.message}</Text></Card>;
-
-            /* Determine number of audits per genre for D3 graph. */
-            const totalComplianceAudits = data.audits.reduce(function (total, audit) {
-                return audit.genre === 'Compliance' ? total + 1 : total
-            }, 0);
-            const totalFinancialAudits = data.audits.reduce(function (total, audit) {
-                return audit.genre === 'Financial' ? total + 1 : total
-            }, 0);
-            const totalInvestigativeAudits = data.audits.reduce(function (total, audit) {
-                return audit.genre === 'Investigative' ? total + 1 : total
-            }, 0);
-            const totalOperationalAudits = data.audits.reduce(function (total, audit) {
-                return audit.genre === 'Operational' ? total + 1 : total
-            }, 0);
-            
-            /* Define chart data. */
-            const auditChartData = [
-                { angle: totalComplianceAudits, radius: 1, label: 'Compliance' },
-                { angle: totalFinancialAudits, radius: 1, label: 'Financial' },
-                { angle: totalInvestigativeAudits, radius: 1, label: 'Investigative' },
-                { angle: totalOperationalAudits, radius: 1, label: 'Operational' }
-            ]
 
             return (
                 <main>
@@ -70,12 +47,7 @@ const AuditList = props => (
                         </Flex>
                         <Box mb={50}>
                             <Heading mb={3}>Audit Genre Frequency</Heading>
-                            <RadialChart
-                                showLabels={true}
-                                data={auditChartData}
-                                width={400}
-                                height={400}
-                            />
+                            <GenreRadialChart data={data} />
                         </Box>
                     </Card>
                 </main>
---
+++ client/src/components/Charts/GenreRadialChart/GenreRadialChart.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { RadialChart } from 'react-vis';
+
+const GenreRadialChart = ({ data }) => {
+
+    /* Determine number of audits per genre for D3 graph. */
+    const totalComplianceAudits = data.audits.reduce(function (total, audit) {
+        return audit.genre === 'Compliance' ? total + 1 : total
+    }, 0);
+    const totalFinancialAudits = data.audits.reduce(function (total, audit) {
+        return audit.genre === 'Financial' ? total + 1 : total
+    }, 0);
+    const totalInvestigativeAudits = data.audits.reduce(function (total, audit) {
+        return audit.genre === 'Investigative' ? total + 1 : total
+    }, 0);
+    const totalOperationalAudits = data.audits.reduce(function (total, audit) {
+        return audit.genre === 'Operational' ? total + 1 : total
+    }, 0);
+
+    /* Define chart data. */
+    const auditChartData = [
+        { angle: totalComplianceAudits, radius: 1, label: 'Compliance' },
+        { angle: totalFinancialAudits, radius: 1, label: 'Financial' },
+        { angle: totalInvestigativeAudits, radius: 1, label: 'Investigative' },
+        { angle: totalOperationalAudits, radius: 1, label: 'Operational' }
+    ]
+
+    return (
+        <RadialChart
+            showLabels={true}
+            data={auditChartData}
+            width={400}
+            height={400}
+        />
+    )
+}
+
+export default GenreRadialChart;
